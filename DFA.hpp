df9fced8784d9076635905a92ae51c3da6b9a2b7
ALINGCAT <2218773842@qq.com>
2022-11-04 16:43:40 +0800

    增加一点注释

--- DFA.hpp
+++ DFA.hpp
@@ -5,14 +5,19 @@
 template<class F>
 class DFA {
 public:
+    // 当前状态
     STATE Current;
+    // 基于初始状态构造DFA
     DFA(STATE start) : Current(start) {}
+    // 增加从origin状态指向target状态的转移路径 定义转移时将会返回的func
     void AddPointer(STATE origin, STATE target, F func) { TransferActions[origin][target] = func; }
+    // 将当前状态转移为目标状态 返回func
     F MoveTo(STATE target) {
         auto func = TransferActions[Current][target];
         Current = target;
         return func;
     }
 private:
+    // 转移func映射
     std::map<STATE, std::map<STATE, F>> TransferActions;
 };
--- DFA_Example.cpp
+++ DFA_Example.cpp
@@ -9,19 +9,31 @@ int main() {
         {'0', true}, {'1', true}, {'2', true}, {'3', true}, {'4', true}, {'5', true}, {'6', true}, {'7', true}, {'8', true}, {'9', true},
         {'[', false}, {']', false}, {',', false}
     };
+    // NUM状态为读入的字符为数码 SEQ状态为读入的字符为符号
     enum State { NUM, SEQ };
+    // 以函数指针类型作为模版类参数 string&记录收集起来的数码 ch为当前需要收集的数码 vector<int>&为所有数收集后的结果集合
     DFA<void(*)(string&, char, vector<int>&)> dfa(SEQ);
+    // 增加从NUM到NUM状态的转移 当发生此转移时 str收集ch数码
     dfa.AddPointer(NUM, NUM, [](string &str, char ch, vector<int> &nums) { str += ch; });
+    // 增加从SEQ到NUM状态的转移 当发生此转移时 str清空并收集第一个ch数码
     dfa.AddPointer(SEQ, NUM, [](string &str, char ch, vector<int> &nums) { str = ch; });
+    // 增加从NUM到SEQ状态的转移 当发生此转移时 将str转换成整型存入nums中
     dfa.AddPointer(NUM, SEQ, [](string &str, char ch, vector<int> &nums) { nums.push_back(stoi(str)); });
+    // 增加从SEQ到SEQ状态的转移 当发生此转移时 将什么事都不做
     dfa.AddPointer(SEQ, SEQ, [](string &str, char ch, vector<int> &nums) {});
+    // str为用于读入输入的字符串 temp用于保存收集起来的数码
     string str, temp = "";
+    // nums为整型结果集合
     vector<int> nums;
     cin >> str;
+    // 遍历输入的字符串
     for (auto iter = str.begin(); iter != str.end(); iter++) {
+        // 当此字符为数码 则将状态转移为NUM 并触发状态转移func
         if (IsNum[*iter]) dfa.MoveTo(NUM)(temp, *iter, nums);
+        // 当此字符为符号 则将状态转移为SEQ 并触发状态转移func
         else dfa.MoveTo(SEQ)(temp, *iter, nums);
     }
+    // 遍历输出nums
     for (auto iter = nums.begin(); iter != nums.end(); iter++) { cout << *iter << " "; }
     return 0;
 }
