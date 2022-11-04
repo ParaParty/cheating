83042e913fb71c9814a0f97f9383298f2dfca958
ALINGCAT <2218773842@qq.com>
2022-11-04 16:09:43 +0800

    DFA测试版v1.1

--- DFA.hpp
+++ DFA.hpp
@@ -1,43 +1,18 @@
 #include <map>
+#include <vector>
+#define STATE std::size_t
 
 template<class T>
 class DFA {
 public:
-    class State {
-    public:
-        void (*Enter)() = []{};
-        void (*Leave)() = []{};
-        // receive the signal will trigger current state to target state
-        bool PointTo(T signal, State *target) {
-            if (IsPointTo(signal)) return false;
-            PointTowards.try_emplace(signal, target);
-            return true;
-        }
-        // whether does the state have way to other state
-        bool IsPointTo(T signal) { return PointTowards.find(signal) != PointTowards.end(); }
-        // trigger the state leave function and target state enter function
-        // return target state
-        // if no way to point target state, there is no thing happen and return self
-        State *Transfer(T signal) {
-            if (!IsPointTo(signal)) return this;
-            Leave();
-            PointTowards[signal]->Enter();
-            return PointTowards[signal];
-        }
-    private:
-        // the state could go to which states
-        // T is a signal to trigger the state to the target state
-        std::map<T, State*> PointTowards;
-    };
-
-    DFA(State *start) : Current(start) {}
-
-    // receive a signal to transfer current state
-    bool Receive(T signal) {
-        if (!Current->IsPointTo(signal)) return false;
-        Current = Current->Transfer(signal);
-        return true;
+    STATE Current;
+    DFA(STATE start) : Current(start) {}
+    void AddPointer(STATE origin, STATE target, T func) { TransferActions[origin][target] = func; }
+    T MoveTo(STATE target) {
+        auto func = TransferActions[Current][target];
+        Current = target;
+        return func;
     }
 private:
-    State *Current;
+    std::map<STATE, std::map<STATE, T>> TransferActions;
 };
--- DFA_Example.cpp
+++ DFA_Example.cpp
@@ -2,30 +2,26 @@
 #include <iostream>
 using namespace std;
 
+string NumberString = "";
+
 int main() {
-    DFA<char>::State a, b, c, d, start;
-    start.PointTo('a' , &a);
-    start.PointTo('b' , &b);
-    start.PointTo('c' , &c);
-    start.PointTo('d' , &d);
-    a.PointTo('b', &b);
-    b.PointTo('c', &c);
-    c.PointTo('d', &d);
-    d.PointTo('a', &a);
-    start.Leave = [] { cout << "leave start "; };
-    a.Enter = [] { cout << "to a" << endl; };
-    a.Leave = [] { cout << "leave a "; };
-    b.Enter = [] { cout << "to b" << endl; };
-    b.Leave = [] { cout << "leave b "; };
-    c.Enter = [] { cout << "to c" << endl; };
-    c.Leave = [] { cout << "leave c "; };
-    d.Enter = [] { cout << "to d " << endl; };
-    d.Leave = [] { cout << "leave d "; };
-    DFA<char> dfa(&start);
-    string str;
+    map<char, bool> IsNum {
+        {'0', true}, {'1', true}, {'2', true}, {'3', true}, {'4', true}, {'5', true}, {'6', true}, {'7', true}, {'8', true}, {'9', true},
+        {'[', false}, {']', false}, {',', false}
+    };
+    enum State { NUM, SEQ };
+    DFA<void(*)(string&, char, vector<int>&)> dfa(SEQ);
+    dfa.AddPointer(NUM, NUM, [](string &str, char ch, vector<int> &nums) { str += ch; });
+    dfa.AddPointer(SEQ, NUM, [](string &str, char ch, vector<int> &nums) { str = ch; });
+    dfa.AddPointer(NUM, SEQ, [](string &str, char ch, vector<int> &nums) { nums.push_back(stoi(str)); });
+    dfa.AddPointer(SEQ, SEQ, [](string &str, char ch, vector<int> &nums) {});
+    string str, temp = "";
+    vector<int> nums;
     cin >> str;
     for (auto iter = str.begin(); iter != str.end(); iter++) {
-        if (!dfa.Receive(*iter)) cout << "Error: Fail to transfer to " << *iter << endl;
+        if (IsNum[*iter]) dfa.MoveTo(NUM)(temp, *iter, nums);
+        else dfa.MoveTo(SEQ)(temp, *iter, nums);
     }
+    for (auto iter = nums.begin(); iter != nums.end(); iter++) { cout << *iter << " "; }
     return 0;
 }
